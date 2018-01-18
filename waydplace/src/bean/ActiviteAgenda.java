3757d80f733d39bc3b747985195106f008a6f391
philippe esi <pmestivier@club.fr>
2018-01-18 16:00:38 +0100

    ok esi

--- waydplace/src/bean/ActiviteAgenda.java
+++ waydplace/src/bean/ActiviteAgenda.java
@@ -1,21 +1,33 @@
 package bean;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class ActiviteAgenda {
  String titre;
  Date dateDebut;
  Date dateFin;
+ 
  int id;
+private Calendar calDebut;
+private Calendar calFin;
  
  
 public ActiviteAgenda(int id,String titre, Date dateDebut, Date dateFin) {
+	
 	super();
 	this.id=id;
 	this.titre = titre;
 	this.dateDebut = dateDebut;
 	this.dateFin = dateFin;
+	
+	 calDebut=Calendar.getInstance();
+	 calFin=Calendar.getInstance();
+	calDebut.setTime(dateDebut);
+	calFin.setTime(dateFin);
+	
 }
+
 public String getTitre() {
 	return titre;
 }
@@ -40,6 +52,56 @@ public int getId() {
 public void setId(int id) {
 	this.id = id;
 }
- 
- 
+
+public int getAnnneDebut(){
+	
+	return  calDebut.get(Calendar.YEAR);
+}
+
+public int getAnnneFin(){
+	
+	return  calFin.get(Calendar.YEAR);
+}
+
+public int getJourDebut(){
+	
+	return  calDebut.get(Calendar.DAY_OF_MONTH);
+}
+
+public int getJourFin(){
+	
+	return  calFin.get(Calendar.DAY_OF_MONTH);
+}
+
+
+public int getMoisDebut(){
+	
+	return  calDebut.get(Calendar.MONTH);
+}
+
+public int getMoisFin(){
+	
+	return  calFin.get(Calendar.MONTH);
+}
+
+public int getHeureDebut(){
+	
+	return  calDebut.get(Calendar.HOUR_OF_DAY);
+}
+
+public int getHeureFin(){
+	
+	return  calFin.get(Calendar.HOUR_OF_DAY);
+}
+
+public int getMinuteDebut(){
+	
+	return  calDebut.get(Calendar.MINUTE);
+}
+
+public int getMinuteFin(){
+	
+	return  calFin.get(Calendar.MINUTE);
+}
+
 }
