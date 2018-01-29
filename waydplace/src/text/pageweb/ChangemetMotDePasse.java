945295597b7187b1391e9204c4968bbeed95e63e
bibou <pmestivier@club.fr>
2018-01-29 06:24:26 +0100

    ok

---
+++ waydplace/src/text/pageweb/ChangemetMotDePasse.java
@@ -0,0 +1,18 @@
+package text.pageweb;
+
+
+
+public class ChangemetMotDePasse {
+
+	
+	public static final String HINT_MOT_DE_PASSE_BIS =  CommunText.HINT_MOT_DE_PASSE_BIS;
+	public static final String HINT_MOT_DE_PASSE = CommunText.HINT_MOT_DE_PASSE;
+	public static final String HINT_ANCIEN_MOT_DE_PASSE =  "Ancien mot de passe";
+	
+	public static String TITRE_PANEL = "Changement de mot de passe";
+	public static String TITRE_ONGLET = "Mot de passe";
+	public static String JUMBO_TITRE="Changement de mot de passe";
+	public static String JUMBO_LIGNE1="";
+
+
+}
